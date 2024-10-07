2790e701e21b7807fa6eceab69fcbdaba39c1ea6
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2024-10-07 09:58:10 +0530

    mysql

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ db_path = "users.db"
 if not os.path.exists(db_path):
     open(db_path, "a").close()  # Create an empty file
 
-db = SQL(f"sqlite:///{db_path}")
+db = SQL(f"mysql://root:BcVikLfbCHymfSaJXfRwJrcirHxJhMlo@junction.proxy.rlwy.net:13443/railway")
 
 
 def init_db():
